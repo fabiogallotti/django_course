cf0f3a886986a4ea0297305eb9c51d0341ad5ed3
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2019-03-27 19:11:47 +0100

    Document Object Model

---
+++ document_object_model/tictactoe.js
@@ -0,0 +1,26 @@
+var restart = document.querySelector('#button')
+var cells = document.querySelectorAll('td')
+
+function clearBoard() {
+  for (var i = 0; i < cells.length; i++) {
+      cells[i].textContent = '';
+  }
+
+}
+
+restart.addEventListener('click',clearBoard)
+
+function changeMarker() {
+  if (this.textContent === '') {
+    this.textContent = 'X';
+    } else if (this.textContent === 'X') {
+      this.textContent = 'O';
+    } else {
+      this.textContent = '';
+    }
+};
+
+// Use a for loop to add Event listeners to all the squares
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', changeMarker);
+}
