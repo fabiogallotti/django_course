29af8ceb3254827ea2464423da447e1864f1bed2
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2019-09-14 17:13:51 +0100

    Django forms

--- django/ProTwo/AppTwo/views.py
+++ django/ProTwo/AppTwo/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from AppTwo.models import User
+from AppTwo.forms import NewUserForm
 # Create your views here.
 
-def index(response):
-    return HttpResponse(
-    """<em>My Second App</em>
-    <h2>Go to /users to see the list of users</h2>
-    """)
+def index(request):
+    return render(request, 'AppTwo/index.html')
 
 def help(request):
     help_dict = {'help_insert': 'Help Page'}
     return render(request, 'AppTwo/help.html', context=help_dict)
 
 def users(request):
-    user_list = User.objects.order_by("last_name")
-    users_dict = {'users': user_list}
-    return render(request, 'AppTwo/users.html', context=users_dict)
+    form = NewUserForm()
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("Error form invalid")
+
+    return render(request, 'AppTwo/users.html', {'form':form})
